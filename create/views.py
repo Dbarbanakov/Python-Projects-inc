6153077e7f76cc957946bd09fd6cb138315c6028
Dbarbanakov <dbarbanakov@gmail.com>
2023-01-14 09:36:00 +0200

    Avatars

--- create/views.py
+++ create/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Hero
+from .models import Hero, Avatar
 from django import forms
 
 
@@ -16,19 +16,30 @@ def index(request):
 
 def hero(request, hero_id):
     form = HeroForm()
+    avatars = Avatar.objects.all()
+    hero = get_object_or_404(Hero, pk=hero_id)
     if request.method == "POST":
-        name = request.POST["name"]
-        hero = get_object_or_404(Hero, pk=hero_id)
-        hero.name = name
-        hero.save()
+        if "new_avatar" in request.POST:
+            new_avatar = request.POST["new_avatar"]
+            hero.avatar = Avatar.objects.get(id=new_avatar)
+        if "name" in request.POST:
+            name = request.POST["name"]
+            hero.name = name
+
+    hero.save()
     hero = get_object_or_404(Hero.objects.filter(pk=hero_id))
-    return render(request, "create/hero.html", {"hero": hero, "form": form})
+    return render(
+        request,
+        "create/hero.html",
+        {"hero": hero, "form": form, "avatars": avatars},
+    )
 
 
 def status(request):
     if request.method == "POST":
-        name = request.POST["name"]
-        new_hero = Hero(name=name)
+        if request.POST["name"]:
+            name = request.POST["name"]
+            new_hero = Hero(name=name)
         new_hero.save()
     heroes = Hero.objects.all()
     return render(request, "create/status.html", {"heroes": heroes})
